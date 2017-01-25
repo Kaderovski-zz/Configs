ee38c62ea56ff166aea7fd6872ea2ebd56803917
F00b4rch <f00b4rch@gmail.com>
2017-01-25 09:58:15 +0100

    Update diag.sh

--- perso/diag.sh
+++ perso/diag.sh
@@ -1,25 +1,44 @@
+
 #!/bin/sh
-# Ce script a pour but de vérifier si le serveur
-# répond au ping et fait un scan de ports sur ce 
-# dernier
-# Dev par Corentin
+# Dev par f00b4rch
 
-
-echo -e "\n\n\n"
+clear
+echo -e "
+  __  ___   ___  _    _  _            _       _____  _             
+ / _|/ _ \ / _ \| |  | || |          | |     |  __ \(_)            
+| |_| | | | | | | |__| || |_ _ __ ___| |__   | |  | |_  __ _  __ _ 
+|  _| | | | | | | '_ \__   _| '__/ __| '_ \  | |  | | |/ _\` |/ _\` |
+| | | |_| | |_| | |_) | | | | | | (__| | | | | |__| | | (_| | (_| |
+|_|  \___/ \___/|_.__/  |_| |_|  \___|_| |_| |_____/|_|\__,_|\__, |
+                                                              __/ |
+                                                             |___/ "
+echo -e '\nNoraj de mon scriptage...'
+echo -e '\n'
 
 read -p "Host/IP : " ip
 
-
-echo -e '\n# OUTPUT=$(host $ip | awk '{print $4}') ; echo "IP : ${OUTPUT}" ; ping $ip -c 5 -i 0.2 ; nmap $ip -Pn \n' 
-
-OUTPUT=$(host $ip | awk '{print $4}')
+OUTPUT=$(host $ip | grep -Eo '[1-9]{1,3}\.([0-9]{1,3}\.){2}[0-9]{1,3}')
+echo -e '\n'
+echo -e '\n'
 echo "IP : ${OUTPUT}" 
 echo -e '\n'
+echo -e '--------------------\nVérification ICMP\n--------------------'
 ping -i 0.2 -c 5 $ip
-#echo -e '\n'
-nmap $ip -Pn 
-
-
+echo -e '\n\n--------------------\nScan via Nmap\n--------------------'
+echo '[...]'
+nmap $ip -Pn
 echo -e '\n'
+echo -e '----------\nDNS => A\n----------'
+dig $ip +short
+echo -e '\n'
+echo -e '-------------------------\nRécupération serveurs NS\n-------------------------'
+echo '[...]'
+sleep 1
+ns=$(whois metal-cunt.com | grep -i "name server" | grep -Eo "([a-zA-Z0-9]+\.){1,}[a-zA-Z0-9]+\.[a-z]{2,}")
+echo $ns
+echo -e '\n'
+echo -e '-------------------------\nDig sur les serveurs NS\n-------------------------'
+echo -e '\n'
+for i in $(echo $ns) ; do echo 'dig' $ip '@'$i ; dig $ip @$i ; done
 
 exit
